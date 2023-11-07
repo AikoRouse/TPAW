e7640bb37256c9f4316f537b6522e123061b823a
Camila A Peralta R <112417026+AikoRouse@users.noreply.github.com>
2023-11-07 11:00:33 -0300

    Add files via upload

---
+++ Principal.js
@@ -0,0 +1,11 @@
+import tienda from "./catalogo.js"
+
+const contenedor = document.getElementById("contendor")
+
+contenedor.innerHTML="Hola Tienda"
+
+let contendorHTML = ""
+
+tienda.articulos.array.forEach(element => {
+    
+});
---
+++ catalogo.js
@@ -0,0 +1,21 @@
+const tienda = {
+    articulos:[
+        {
+            imagen: "CSS/imagen1.jpg",
+            titulo: "GROOT MACETA",
+            precio:"$7000.00",
+        },
+        {
+            imagen: "CSS/imagen2.jpg",
+            titulo: "LLAMA YOGA",
+            precio:"$1500.00",
+        },
+        {
+            imagen: "CSS/imagen3.jpg",
+            titulo: "SPIDERMAN",
+            precio:"$7000.00",
+        }
+    ]
+}
+
+export default tienda
